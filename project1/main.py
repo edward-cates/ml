33bbdae2a7f9df8f3a3441d93263c094993f0f5f
Edward Cates <edward@getpopspots.com>
2018-09-21 00:28:54 -0500

    shit

--- project1/main.py
+++ project1/main.py
@@ -1,17 +1,22 @@
 import math
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from sklearn.dummy import DummyClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import LinearSVC
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import resample
+
+from graph import plot_learning_curve
 
 matplotlib.rcParams.update({'font.size': 6})
 fig = plt.figure()
@@ -19,20 +24,20 @@ fig = plt.figure()
 # data = pd.read_csv('absent.csv')
 data = pd.read_csv('cc-defaults.csv')
 # up-sample (https://elitedatascience.com/imbalanced-classes)
-y = data.ix[:, -1]
-df_maj = data[y == 0]
-df_min = data[y == 1]
-df_min_up = resample(df_min, replace = True, n_samples = df_maj.shape[0])
-data = pd.concat([df_maj, df_min_up])
+# y = data.ix[:, -1]
+# df_maj = data[y == 0]
+# df_min = data[y == 1]
+# df_min_up = resample(df_min, replace = True, n_samples = df_maj.shape[0])
+# data = pd.concat([df_maj, df_min_up])
 
 x = data.ix[:, :-1].values
 y = data.ix[:, -1].values
 
 algos = [
-  # {
-  #   "label": "Dummy",
-  #   "learner": DummyClassifier(),
-  # },
+  {
+    "label": "Dummy",
+    "learner": DummyClassifier(),
+  },
   {
     "label": "Boosting",
     "learner": AdaBoostClassifier(),
@@ -47,23 +52,26 @@ algos = [
   },
   {
     "label": "Neural Network",
-    "learner": MLPClassifier(),
+    "learner": MLPClassifier(tol=1e-6,learning_rate_init=0.001),#hidden_layer_sizes=(1000,500,100,50)
   },
-  # {
-  #   "label": "SVM",
-  #   "learner": SVC(kernel='linear', tol=1),
-  # },
+  {
+    "label": "SVM",
+    # "learner": SVC(kernel='linear', tol=1),
+    "learner": LinearSVC(dual=False, max_iter=9999999),
+  },
 ]
 
 rows = []
-for i in range(0, 9, 1):
-  rows.append(5000 * (1 + i))
+for i in range(0, 10, 1):
+  rows.append(100 * (i + 1))
 
 count = 0
 max = len(algos)
 
 for algo in algos:
   print(algo["label"])
+
+  plot_learning_curve(algo["learner"], algo["label"], x, y, n_jobs=8, train_sizes=rows)
 
   n_rows = []
   train_errors = []
@@ -72,29 +80,33 @@ for algo in algos:
 
   for total_rows in rows:
     print(total_rows)
-    # used to get a subset of the data of size total_rows
-    sss1 = StratifiedShuffleSplit(n_splits=1, test_size=(total_rows/x.shape[0]))
-    for train_index, test_index in sss1.split(x, y):
-      # tmp data subset of size total_rows
-      tmp_x, tmp_y = x[test_index], y[test_index]
+    base_ratio = total_rows / x.shape[0]
 
-      sss2 = StratifiedShuffleSplit(n_splits=1, test_size=.3)
-      for train_index2, test_index2 in sss2.split(tmp_x, tmp_y):
-        train_x, train_y = tmp_x[train_index2], tmp_y[train_index2]
-        test_x, test_y = tmp_x[test_index2], tmp_y[test_index2]
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=.3 * base_ratio, train_size=.7 * base_ratio)
 
-        algo["learner"].fit(train_x, train_y)
-        train_score = algo["learner"].score(train_x, train_y)
-        test_score = algo["learner"].score(test_x, test_y)
+    train_data = np.append(train_x, np.zeros((train_x.shape[0], 1)), axis=1)
+    train_data[:, -1] = train_y
 
-        n_rows.append(total_rows)
-        train_errors.append(1 - train_score)
-        test_errors.append(1 - test_score)
-      #endfor
+    df_maj = train_data[train_y == 0]
+    df_min = train_data[train_y == 1]
+    df_min_up = resample(df_min, replace = True, n_samples = df_maj.shape[0])
+    train_data = np.append(df_maj, df_min_up, axis=0)
 
-      cv_score = cross_val_score(algo["learner"], tmp_x, tmp_y, cv=10).mean()
-      cross_val_errors.append(1 - cv_score)
-    #endfor
+    train_x = train_data[:, :-1]
+    train_y = train_data[:, -1]
+
+    algo["learner"].fit(train_x, train_y)
+    train_score = algo["learner"].score(train_x, train_y)
+    test_score = algo["learner"].score(test_x, test_y)
+
+    n_rows.append(total_rows)
+    train_errors.append(1 - train_score)
+    test_errors.append(1 - test_score)
+
+    tmp_x = np.append(train_x, test_x, axis=0)
+    tmp_y = np.append(train_y, test_y, axis=0)
+    cv_score = cross_val_score(algo["learner"], tmp_x, tmp_y, cv=10).mean()
+    cross_val_errors.append(1 - cv_score)
   #endfor
 
   count += 1
@@ -102,9 +114,9 @@ for algo in algos:
   plt.set_title(algo["label"])
   plt.set_xlabel("Instances")
   plt.set_ylabel("Error")
-  plt.plot(n_rows, train_errors, label="Train")
-  plt.plot(n_rows, test_errors, label="Test")
-  plt.plot(n_rows, cross_val_errors, label="Cross Val")
+  plt.plot(n_rows, train_errors, "o-", label="Train")
+  # plt.plot(n_rows, test_errors, label="Test")
+  plt.plot(n_rows, cross_val_errors, "o-", label="Cross Val")
   plt.legend()
 #endfor
 
